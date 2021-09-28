25dfeff12df4703484c2bd10e9cf3c5b8a4dcded
woong97 <yjwoong97@gmail.com>
2021-09-29 02:43:59 +0900

    error handling for input arguments of sys_ptree

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,11 +1,28 @@
 #include <linux/kernel.h>
 #include <linux/prinfo.h>
-
+#include <linux/unistd.h>
+//#include <uapi/asm-generic/errno-base.h>
+#include <linux/uaccess.h>
 
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr){
-	int a;
-	a = *nr + 1;
-	return a;
+	int tmp_nr;
+	struct prinfo tmp_prinfo;
+
+	if (buf == NULL || nr == NULL)
+		return -EINVAL;
+
+	// need to check: access_ok just check availabilty of scope of address
+	// So we will use copy_from_user
+	// need check: copy_from_user is the most powerful among
+	// copy_from user, get_user, access_ok
+	if (copy_from_user(&tmp_nr, nr, sizeof(tmp_nr)))
+		return -EFAULT;
+	if (copy_from_user(&tmp_prinfo, buf, sizeof(tmp_prinfo)))
+		return -EFAULT;
+	if (tmp_nr < 1) {
+		return -EINVAL;
+	}
+	return *nr;
 
 }
 
--- test.c
+++ test.c
@@ -3,12 +3,40 @@
 #include <syscall.h>
 #include "include/linux/prinfo.h"
 #include <stdlib.h>
+#include <errno.h>
 
-int main(){
-	int nr = 5;
+#define PTREE_SYS_CALL_NUM	398
+#define DEFAULT_NR		100
+
+
+int main(int argc, char* argv[]){
+	int nr;
+	int num_process;
+	if (argc > 2) {
+		perror("Too many input");
+		exit(1);
+	} else if (argc == 2) {
+		nr = atoi(argv[1]);
+	} else {
+		nr = DEFAULT_NR;
+	}
+
 	struct prinfo *buf = (struct prinfo *)malloc(sizeof(struct prinfo) * nr);
-	int num_process = syscall(398, t, &nr);
+	num_process = syscall(PTREE_SYS_CALL_NUM, buf, &nr);
+	if (num_process < 0){
+		if (errno == EFAULT){
+			perror("Fail access User memory address");
+		} else if (errno == EINVAL) {
+			perror("Pointer input is null or nr is lower than 1");
+		} else {
+			fprintf(stderr, "Value of errno: %d\n", errno);
+			perror("syscall() fail");
+		}
+		exit(1);	
+	};
+
 	printf("Total number of process: %d\n", num_process);
 	free(buf);
+	return 0;
 }
 

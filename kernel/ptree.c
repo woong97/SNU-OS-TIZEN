d8ed58166f7742f568881c3176aa858fa66f95b8
sndjg <hurwun0103@snu.ac.kr>
2021-09-28 21:39:47 +0900

    change "" to <> in kernel/ptree.c

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,9 +1,16 @@
 //#include "stdio.h"
 #include <linux/kernel.h>
-#include "linux/prinfo.h"
+#include <linux/prinfo.h>
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr){
 	int a;
 	a = *nr + 1;
 	printk("Hello ptree.c\n");
 	return a;
+
 }
+
+/*
+ * asmlinkabe: for i386 architecture
+ * printk: <linux/printk.h>
+ * why not <linux/prinfo.h>?
+ */

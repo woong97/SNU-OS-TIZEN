ac185956b056254a43137a51820529eea63a41da
sndjg <hurwun0103@snu.ac.kr>
2021-10-01 14:10:20 +0900

    Add prinfo_print function
    This function prints list of processes in tree structure.

--- test.c
+++ test.c
@@ -8,6 +8,7 @@
 #define PTREE_SYS_CALL_NUM	398
 #define DEFAULT_NR		100
 
+void prinfo_print(struct prinfo *buf, int nr); // pre-order printing of ptree
 
 int main(int argc, char* argv[]){
 	int nr;
@@ -36,7 +37,39 @@ int main(int argc, char* argv[]){
 	};
 
 	printf("Total number of process: %d\n", num_process);
+	prinfo_print(buf, nr);
 	free(buf);
 	return 0;
 }
 
+void prinfo_print(struct prinfo *buf, int nr) {
+	// pid_list and level_tracker for indentation
+	pid_t *pid_list = (pid_t *)malloc(sizeof(pid_t)*nr);
+	int *level_list = (int *)malloc(sizeof(int)*nr);
+	int current_level = 0;
+
+	int i = 0, j = 0; // indexing variable
+	struct prinfo p = buf[i];
+	int prev = -1;
+	printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid,
+		p.first_child_pid, p.next_sibling_pid, p.uid);
+	pid_list[i] = p.pid;
+	level_list[i] = current_level;
+	for(i = 1; i < nr; i++) {
+		p = buf[i];
+		prev = i-1;						// used for finding current_level
+		if(buf[prev].first_child_pid == p.pid)	// p is child of buf[prev]
+			current_level++;					// increment level
+		else if(buf[prev].next_sibling_pid == p.pid)	// p is sibling of buf[prev]
+			current_level;								// do nothing
+		else {									// case 3
+			while(prev >= 0 &&
+				p.parent_pid != buf[prev].pid) prev--;	// find parent
+			if(prev < 0) current_level = 0;				// no parent => level = 0
+			else current_level = level_list[prev]+1;	// level = level[parent]+1
+		}
+		for(j = 0; j < current_level; j++) printf("  ");
+		printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state,
+			p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+	}
+}

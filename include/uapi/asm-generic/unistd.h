255f752bc3ccca3c1f70dd603da04d70d42240bc
woong97 <yjwoong97@gmail.com>
2021-10-04 02:18:15 +0900

    remove unnecessary part register syscall

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -733,10 +733,8 @@ __SYSCALL(__NR_pkey_free,     sys_pkey_free)
 #define __NR_statx 291
 __SYSCALL(__NR_statx,     sys_statx)
 
-#define __NR_sys_ptree 398
-__SYSCALL(__NR_sys_ptree, sys_ptree)
 #undef __NR_syscalls
-#define __NR_syscalls 399
+#define __NR_syscalls 292
 
 /*
  * All syscalls below here should go away really,

3b6114d54467e9c85b9295d8eb2abe03a5886eea
woong97 <yjwoong97@gmail.com>
2021-11-14 06:40:03 +0900

    add system call

---
+++ kernel/rotation.c
@@ -0,0 +1,32 @@
+
+
+asmlinkage long sys_set_rotation(int degree)
+{
+	// TODO
+	return 0;
+}
+
+asmlinkage long sys_rotlock_read(int degree, int range)
+{
+	// TODO
+	return 0;
+}
+
+asmlinkage long sys_rotlock_write(int degree, int range)
+{
+	// TODO
+	return 0;
+}
+
+asmlinkage long sys_rotunlock_read(int degree, int range)
+{
+	// TODO
+	return 0;
+}
+
+asmlinkage long sys_rotunlock_write(int degree, int rage)
+{
+	// TODO
+	return 0;
+}
+

1c6db393184012f24868f28674de4b2b4b1cc6b8
woong97 <yjwoong97@gmail.com>
2021-11-16 02:59:43 +0900

    start test code(not finished)

--- test/selector.c
+++ test/selector.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define	FILE_PATH		"integer"
+#define	SYSCALL_ROTLOCK_WRITE	400
+#define SYSCALL_ROTUNLOCK_WRITE	402
+
+
 int main(int argc, char *argv[])
 {
-	// TODO
+	if (argc != 2) {
+		printf("Input should be ./selector <integer>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int number = atoi(argv[1]);
+	FILE *fp = fopen(FILE_PATH, "w");
+
+	// TEST WRITE
+	// TODO - WRITELOCK
+	fprintf(fp, "%d", number);
+	fclose(fp);
+	printf("Write Succeed\n");
 	return 0;
 }
--- test/trial.c
+++ test/trial.c
@@ -1,8 +1,59 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define FILE_PATH		"integer"
+#define SYSCALL_ROTLOCK_READ	399
+#define SYSCALL_ROTUNLOCK_READ	401
+
+
+int naive_factorization(int number, int idx)
+{
+	if (number <= 0) return -EINVAL;
+	if (number == 1) {
+		printf("1 = 1\n");
+		return EXIT_SUCCESS;
+	}
+	int original_number = number;
+	char *output = "";
+	for (int i = 2; i < original_number; i++) {
+		if ((number % i) == 0) {
+			number /= i;
+			if (strlen(output) == 0) {
+				if(asprintf(&output, "%d", i) == -1) return -ENOMEM;
+			} else {
+				if(asprintf(&output, "%s * %d", output, i) == -1) return -ENOMEM;
+			}
+			i--;
+		}
+	}
+	
+	if (original_number == number)
+		if(asprintf(&output, "%d", number) == -1) return -ENOMEM;
+	printf("trial-%d: %d = %s\n", idx, original_number, output);
+	return EXIT_SUCCESS;
+}
 
 int main(int argc, char *argv[])
 {
 	// TODO
-	return 0;
+	if (argc != 2) {
+		printf("Input should be ./integer <process index>\n");
+		exit(EXIT_FAILURE);
+	}
+	int idx = atoi(argv[1]);
+	int number;
+	
+	// TEST READ!
+	// TODO - READLOCK
+	FILE *fp = fopen(FILE_PATH, "r");
+	fscanf(fp, "%d", &number);
+	fclose(fp);
+	if (naive_factorization(number, idx) < 0) {
+		perror("Factoraization Failed");
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }

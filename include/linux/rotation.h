16542fc9ab138f76cc6b85c04dc543347b7db2af
woong97 <yjwoong97@gmail.com>
2021-11-16 04:40:24 +0900

    start set degree - not finished

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,1 +1,2 @@
-// TODO
+#define	MIN_DEGREE	0
+#define MAX_DEGREE	359
--- kernel/rotation.c
+++ kernel/rotation.c
@@ -1,9 +1,27 @@
 #include <linux/kernel.h>
+#include <linux/rotation.h>
+#include <linux/mutex.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+
+DEFINE_MUTEX(base_lock);
+DECLARE_WAIT_QUEUE_HEAD(wait_queue);
+
+int curr_degree = 0;
 
 asmlinkage long sys_set_rotation(int degree)
 {
+	int process_awoken = 0;
+	if (degree > MAX_DEGREE || degree < MIN_DEGREE)
+	       return -1;	
+	mutex_lock(&base_lock);
+	curr_degree = degree;
+	wake_up_all(&wait_queue);
 	// TODO
-	return 0;
+	// Find count of waiting process
+	mutex_unlock(&base_lock);
+	
+	return process_awoken;
 }
 
 asmlinkage long sys_rotlock_read(int degree, int range)

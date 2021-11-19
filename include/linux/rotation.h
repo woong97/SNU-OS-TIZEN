cd3ba544b1a0daeaad65ecd257f1b297b3bddd64
woong97 <yjwoong97@gmail.com>
2021-11-20 03:23:27 +0900

    set rotation finished

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,2 +1,17 @@
+#include <linux/list.h>
+
 #define	MIN_DEGREE	0
 #define MAX_DEGREE	359
+#define END_DEGREE	360
+
+#define RT_READ		0
+#define RT_WRITE	1
+
+typedef struct __rotation_range_descriptor {
+	pid_t pid;
+	int degree;
+	int range;
+	int rw_type;
+	struct list_head list;
+} rotation_rd;
+
--- kernel/rotation.c
+++ kernel/rotation.c
@@ -4,23 +4,67 @@
 #include <linux/wait.h>
 #include <linux/sched.h>
 
+/*
+ typedef struct struct __rotation_rane_descriptr {
+ 	pid_t pid;
+	int degree;
+	int range;
+	int rw_type;	// RT_READ or RT_WRITE
+	struct list_head list;
+ } rotation_rd;
+ */
+
+#define	GET_BEGIN(x, y)		(((x) >= (y)) ? ((x) - (y)) : ((x) - (y) + END_DEGREE))
+#define GET_END(x, y)		(((x) + (y)) % END_DEGREE)
+
+
 DEFINE_MUTEX(base_lock);
 DECLARE_WAIT_QUEUE_HEAD(wait_queue);
-
+LIST_HEAD(rotation_rd_list);
 int curr_degree = 0;
+
+
+int is_valid_degree(int begin, int degree, int end) {
+	if (degree >= begin && degree <= end) {
+		return 1;
+	} else {
+		if ((degree >= MIN_DEGREE && degree <= begin) || (degree >= begin && degree <= MAX_DEGREE)) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int find_awoken_count(int degree) {
+	rotation_rd *rd, *tmp;
+	int awoken_cnt = 0;
+	int begin;
+	int end;
+	list_for_each_entry_safe(rd, tmp, &rotation_rd_list, list) {
+		begin = GET_BEGIN(rd->degree, rd->range);
+		end = GET_END(rd->degree, rd->range);
+		if (is_valid_degree(begin, degree, end) == 1) {
+			if (rd->rw_type == RT_WRITE) {
+				return 1;
+			} else if (rd->rw_type == RT_READ) {
+				awoken_cnt += 1;
+			}
+		}
+	}
+	return awoken_cnt;
+}
 
 asmlinkage long sys_set_rotation(int degree)
 {
 	int process_awoken = 0;
 	if (degree > MAX_DEGREE || degree < MIN_DEGREE)
 	       return -1;	
-	mutex_lock(&base_lock);
 	curr_degree = degree;
+	mutex_lock(&base_lock);
 	wake_up_all(&wait_queue);
-	// TODO
-	// Find count of waiting process
+	process_awoken = find_awoken_count(degree);
+	// printk("===process awoken: %d\n", process_awoken);
 	mutex_unlock(&base_lock);
-	
 	return process_awoken;
 }
 

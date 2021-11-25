bd7e550e9c309e781060a363981b1bc54b8cd3cc
woong97 <yjwoong97@gmail.com>
2021-11-26 02:55:46 +0900

    adapt wake_up_process

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -28,6 +28,7 @@ DEFINE_MUTEX(rot_rd_list_mutex);
 int curr_degree = 0;
 atomic_t read_cnt = ATOMIC_INIT(0);
 atomic_t write_cnt = ATOMIC_INIT(0);
+struct task_struct *write_first_queued = NULL;
 
 int is_valid_degree(int begin, int degree, int end) {
 	if (degree >= begin && degree <= end) {
@@ -90,21 +91,13 @@ void remove_rd(pid_t pid, int degree, int range, int rw_type) {
 	mutex_unlock(&rot_rd_list_mutex);
 }
 
-void rot_cv_wait(wait_queue_head_t *wait_queue, struct mutex *mtx) {
+void rotlock_sleep(wait_queue_head_t *wait_queue, struct mutex *mtx) {
 	DEFINE_WAIT(wait);
 	prepare_to_wait(wait_queue, &wait, TASK_INTERRUPTIBLE);
 	mutex_unlock(mtx);
 	schedule();
 	mutex_lock(mtx);
 	finish_wait(wait_queue, &wait);
-}
-
-void rot_cv_signal(wait_queue_head_t *wait_queue, int rw_type) {
-	if (rw_type == RT_READ) {
-		wake_up(wait_queue);
-	} else if (rw_type == RT_WRITE) {
-		wake_up_all(wait_queue);
-	}
 }
 
 static int is_valid_input(int degree, int range) {
@@ -142,7 +135,7 @@ asmlinkage long sys_rotlock_read(int degree, int range)
 			is_valid_degree(GET_BEGIN(rd->degree, rd->range), curr_degree, GET_END(rd->degree, rd->range))) {
 			break;
 		} else {
-			rot_cv_wait(&wait_queue_cv, &base_lock);
+			rotlock_sleep(&wait_queue_cv, &base_lock);
 		}
 	}
 	atomic_add(1, &read_cnt);
@@ -162,12 +155,13 @@ asmlinkage long sys_rotlock_write(int degree, int range)
 		if ((atomic_read(&write_cnt) == 0) &&
 			is_valid_degree(GET_BEGIN(rd->degree, rd->range), curr_degree, GET_END(rd->degree, rd->range))) {
 			atomic_add(1, &write_cnt);
+			write_first_queued = current;	
 			while(atomic_read(&read_cnt) > 0) {
-				rot_cv_wait(&wait_queue_cv, &base_lock);
+				rotlock_sleep(&wait_queue_cv, &base_lock);
 			}
 			break;
 		} else {
-			rot_cv_wait(&wait_queue_cv, &base_lock);
+			rotlock_sleep(&wait_queue_cv, &base_lock);
 		}
 	}
 	mutex_unlock(&base_lock);
@@ -177,16 +171,19 @@ asmlinkage long sys_rotlock_write(int degree, int range)
 asmlinkage long sys_rotunlock_read(int degree, int range)
 {
 	if (is_valid_input(degree, range) == -1) return -EINVAL;
-	/* I'm not sure we need this (wait in unlock) */
-	///////////////////////////////////////////////
 	mutex_lock(&base_lock);
 	while (is_valid_degree(GET_BEGIN(degree, range), curr_degree, GET_END(degree, range)) != 1) {
-		rot_cv_wait(&wait_queue_cv, &base_lock);
+		rotlock_sleep(&wait_queue_cv, &base_lock);
 	}
-	/////////////////////////////////////////////
-	
 	remove_rd(current->pid, degree, range, RT_READ);
-	rot_cv_signal(&wait_queue_cv, RT_READ);
+	
+	// Wake up	
+	if (atomic_read(&write_cnt) > 0 && atomic_read(&read_cnt) == 0){
+		wake_up_process(write_first_queued);
+	} else {
+		wake_up_all(&wait_queue_cv);
+	}
+
 	mutex_unlock(&base_lock);
 	return 0;
 }
@@ -194,16 +191,14 @@ asmlinkage long sys_rotunlock_read(int degree, int range)
 asmlinkage long sys_rotunlock_write(int degree, int range)
 {
 	if (is_valid_input(degree, range) == -1) return -EINVAL;
-	/* I'm not sure we need this (wait in unlock) */
-	///////////////////////////////////////////////
 	mutex_lock(&base_lock);
 	while (is_valid_degree(GET_BEGIN(degree, range), curr_degree, GET_END(degree, range)) != 1) {
-		rot_cv_wait(&wait_queue_cv, &base_lock);
+		rotlock_sleep(&wait_queue_cv, &base_lock);
 	}
-	/////////////////////////////////////////////
 	
 	remove_rd(current->pid, degree, range, RT_WRITE);
-	rot_cv_signal(&wait_queue_cv, RT_WRITE);
+	// wake up
+	wake_up_all(&wait_queue_cv);
 	mutex_unlock(&base_lock);
 	return 0;
 }

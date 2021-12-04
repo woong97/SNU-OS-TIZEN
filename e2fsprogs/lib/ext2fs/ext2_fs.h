d4457cb7aefd9c92d03cb563d73c47f22fbc82f5
woong97 <yjwoong97@gmail.com>
2021-12-04 21:46:41 +0900

    add neccessary field in e2fsprogs

--- e2fsprogs/lib/ext2fs/ext2_fs.h
+++ e2fsprogs/lib/ext2fs/ext2_fs.h
@@ -444,6 +444,12 @@ struct ext2_inode {
 			__u32	h_i_author;
 		} hurd2;
 	} osd2;				/* OS dependent 2 */
+
+	__u32 i_lat_integer;
+	__u32 i_lat_fractional;
+	__u32 i_lng_integer;
+	__u32 i_lng_fractional;
+	__u32 i_accuracy;
 };
 
 /*
@@ -501,6 +507,13 @@ struct ext2_inode_large {
 	__u32	i_crtime_extra;	/* extra File creation time (nsec << 2 | epoch)*/
 	__u32	i_version_hi;	/* high 32 bits for 64-bit version */
 /*9c*/	__u32   i_projid;       /* Project ID */
+
+	__u32 i_lat_integer;
+	__u32 i_lat_fractional;
+	__u32 i_lng_integer;
+	__u32 i_lng_fractional;
+	__u32 i_accuracy;
+
 };
 
 #define EXT4_INODE_CSUM_HI_EXTRA_END	\
